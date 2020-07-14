3cf8b4273a09bc7fb571d2c4e2a39e4a0bba1c33
phatnguyenuit <phatnt.uit@gmail.com>
2020-07-14 15:56:47 +0700

    Update tsconfig of packages/plugin

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -1,30 +1,23 @@
-interface Student {
-  name: string;
-  age: number;
-  email?: string;
-}
-
+import { Student } from './typings/common';
 import './style.css';
 
 console.log('index.ts');
 
 const root = document.getElementById('root');
 
-if (root) {
-  const p = document.createElement('p');
-  p.innerHTML = 'Hello TypeScript!';
-  p.className = 'hello hello-ts';
+const p = document.createElement('p');
+p.innerHTML = 'Hello TypeScript!';
+p.className = 'hello hello-ts';
 
-  root.append(p);
+root?.append(p);
 
-  const student: Student = {
-    name: 'Phat',
-    age: 25,
-    email: 'phatnt.uit@gmail.com',
-  };
+const student: Student = {
+  name: 'Phat',
+  age: 25,
+  email: 'phatnt.uit@gmail.com',
+};
 
-  const div = document.createElement('div');
-  div.innerHTML = JSON.stringify(student, null, 2);
+const div = document.createElement('div');
+div.innerHTML = JSON.stringify(student, null, 2);
 
-  root.append(div);
-}
+root?.append(div);
